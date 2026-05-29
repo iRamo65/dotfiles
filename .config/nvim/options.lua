19a31ee99fcc39d87ae6c05190f5335f167b77f0
iRamo65 <109851700+iRamo65@users.noreply.github.com>
2026-05-29 12:10:52 +0300

    Add files via upload

---
+++ .config/nvim/options.lua
@@ -0,0 +1,10 @@
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.wo.linebreak = true
+vim.opt.smartindent = true
+
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.incsearch = true
+vim.o.hlsearch = true
